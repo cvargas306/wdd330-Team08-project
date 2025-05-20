85a64f90c2afab39d9a20cf6a3a71dd7806c88e2
DonnyDianderas <donnydianderas@gmail.com>
2025-05-19 23:35:08 -0500

    Remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,44 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  addRemoveListeners();
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  return `<li class="cart-card divider">
+    <span class="remove-item" data-id="${item.Id}" style="float:right;cursor:pointer;color:red;font-weight:bold;">✖</span>
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
+}
 
-  return newItem;
+function addRemoveListeners() {
+  const removeButtons = document.querySelectorAll(".remove-item");
+
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      
+      const idToRemove = button.dataset.id;
+      let cartItems = getLocalStorage("so-cart") || [];
+
+      cartItems = cartItems.filter((item) => item.Id !== idToRemove);
+
+      setLocalStorage("so-cart", cartItems);
+
+      renderCartContents();
+    });
+  });
 }
 
 renderCartContents();
