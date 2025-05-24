481a34575e900dca37b012ac5281dbf606c0f555
DonnyDianderas <donnydianderas@gmail.com>
2025-05-23 23:12:10 -0500

    Dynamic Header and Footer + Strech Activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,50 +1,8 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { loadHeaderFooter } from './utils.mjs';
+import ShoppingCart from './shoppingCart.mjs';
 
-// cart/index.js or main.js
-import { updateCartCount } from "../js/utils.mjs";
+loadHeaderFooter();
 
-document.addEventListener("DOMContentLoaded", updateCartCount);
-
-
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart") || [];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-
-  addRemoveListeners();
-}
-
-function cartItemTemplate(item) {
-  return `<li class="cart-card divider">
-    <span class="remove-item" data-id="${item.Id}" style="float:right;cursor:pointer;color:red;font-weight:bold;">✖</span>
-    <a href="#" class="cart-card__image">
-      <img src="${item.Image}" alt="${item.Name}" />
-    </a>
-    <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice}</p>
-  </li>`;
-}
-
-function addRemoveListeners() {
-  const removeButtons = document.querySelectorAll(".remove-item");
-
-  removeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      
-      const idToRemove = button.dataset.id;
-      let cartItems = getLocalStorage("so-cart") || [];
-
-      cartItems = cartItems.filter((item) => item.Id !== idToRemove);
-
-      setLocalStorage("so-cart", cartItems);
-
-      renderCartContents();
-    });
-  });
-}
-
-renderCartContents();
+// Load cart
+const cart = new ShoppingCart();
+cart.init();
---
+++ src/js/shoppingCart.mjs
@@ -0,0 +1,55 @@
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { renderListWithTemplate } from './utils.mjs';
+
+export default class ShoppingCart {
+  constructor() {
+    this.cartItems = getLocalStorage('so-cart') || [];
+  }
+
+  init() {
+    this.renderCartContents();
+  }
+
+  renderCartContents() {
+    const cartListElement = document.querySelector('.product-list');
+    if (cartListElement) {
+      renderListWithTemplate(this.cartItemTemplate.bind(this),cartListElement,this.cartItems,'afterbegin',true );
+      this.addRemoveListeners(); 
+    }
+  }
+
+  cartItemTemplate(item) {
+    return `
+      <li class="cart-card divider">
+        <span class="remove-item" data-id="${item.Id}" style="float:right;cursor:pointer;color:red;font-weight:bold;">✖</span>
+        <a href="#" class="cart-card__image">
+          <img src="${item.Image}" alt="${item.Name}">
+        </a>
+        <a href="#">
+          <h2 class="card__name">${item.Name}</h2>
+        </a>
+        <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+        <p class="cart-card__quantity">qty: 1</p>
+        <p class="cart-card__price">$${item.FinalPrice}</p>
+      </li>
+    `;
+  }
+
+  addRemoveListeners() {
+    document.querySelectorAll('.remove-item').forEach(button => {
+      button.addEventListener('click', (e) => {e.preventDefault();this.removeItem(e.target.dataset.id);});
+    });
+  }
+
+  removeItem(id) {
+    // Filter item to delete
+    this.cartItems = this.cartItems.filter(item => item.Id !== id);
+    
+    // Update localStorage
+    setLocalStorage('so-cart', this.cartItems);
+    
+    this.renderCartContents();
+    
+    document.dispatchEvent(new CustomEvent('cartUpdated'));
+  }
+}
